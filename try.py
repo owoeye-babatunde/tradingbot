ecc2e84d4cc3006e00fc911685f75984a60be34b
Babatunde <owoeyebabatunde01@gmail.com>
2023-09-14 10:52:26 -0700

    first commit

---
+++ try.py
@@ -0,0 +1,111 @@
+"""
+import pandas as pd
+
+# Load historical price data
+data = pd.read_csv("health_price.csv")
+
+# Filter for healthcare sector and market cap
+healthcare_stocks = data[(data['Sector'] == 'Healthcare') & (data['MarketCap'] >= 1e9)]
+# Calculate daily price gaps
+healthcare_stocks['PriceGap'] = healthcare_stocks['Open'] - healthcare_stocks['Close'].shift(1)
+
+# Identify gap-up and gap-down stocks
+gap_up_stocks = healthcare_stocks[healthcare_stocks['PriceGap'] > 0]
+gap_down_stocks = healthcare_stocks[healthcare_stocks['PriceGap'] < 0]
+
+import time
+
+while True:
+    # Implement real-time monitoring logic here
+    # Check for entry conditions, execute trades, and track performance
+    time.sleep(60)  # Sleep for 60 seconds before checking again
+
+# Pseudocode for trade execution
+if stock meets entry conditions:
+    calculate_position_size()
+    determine_strike_price()
+    place_option_order()
+    track_trade_performance()
+
+
+
+# Pseudocode for tracking trade performance
+def track_trade_performance():
+    # Calculate trade performance metrics
+    update_max_drawdown()
+    update_sharpe_ratio()
+    update_percent_gain()
+    log_trade_details()
+
+"""
+
+import pandas as pd
+import streamlit as st
+
+# Load historical price data
+data = pd.read_csv("data_with_close.csv")
+
+# Define algorithm parameters
+# You can define your gap detection logic, market cap filter, and other conditions here.
+# For simplicity, we'll assume some example conditions.
+
+market_cap_threshold = 1e9  # 1 billion USD market cap
+gap_percentage_threshold = 5  # 5% price gap
+position_size = 500  # Maximum position size
+max_utilized_capital = 5000  # Maximum capital to be utilized
+
+# Streamlit web app
+st.title("S&P Healthcare Sector Trading Algorithm Backtesting Result")
+
+# Create a function for the trading algorithm
+def run_algorithm(data):
+    capital = max_utilized_capital  # Initialize available capital
+    positions = []  # Store current positions
+
+    for index, row in data.iterrows():
+        # Implement your gap detection and market cap filtering logic here
+
+
+        row['PriceGap'] = row['Open'] - row['Close']
+
+        # Identify gap-up and gap-down stocks
+        gap_up_stocks = row['PriceGap'] > 0
+        gap_down_stocks = row['PriceGap'] < 0
+
+
+        # For simplicity, let's assume we have detected a suitable stock to trade
+        if gap_up_stocks:
+            if capital >= position_size:
+                # Calculate the number of contracts to buy based on available capital and position size
+                contracts_to_buy = (capital // position_size)
+                # Deduct the utilized capital from available capital
+                utilized_capital = contracts_to_buy * position_size
+                capital -= utilized_capital
+                # Record the position
+                positions.append({
+                    #'StockSymbol': "S&P",
+                    'Contracts': contracts_to_buy,
+                    'EntryPrice': row['Open'],
+                    'ExitPrice': row['Close']
+                })
+
+    return positions
+
+# Create a button to start the algorithm
+if st.button("Start Algorithm"):
+    st.write("Algorithm is running...")
+    positions = run_algorithm(data)
+    st.write("Positions:", positions)
+
+# Create a button to stop the algorithm
+if st.button("Stop Algorithm"):
+    st.write("Algorithm has been stopped.")
+
+# Display real-time updates (placeholder)
+st.write("Real-time updates go here.")
+
+# Display performance metrics (placeholder)
+st.write("Performance Metrics:")
+# Calculate and display maximum drawdown, Sharpe ratio, % gain, etc.
+
+# Display other relevant information as needed
